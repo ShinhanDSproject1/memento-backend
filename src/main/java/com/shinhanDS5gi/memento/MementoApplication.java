005b37a3c6d8392a14e3e5a5ad2472ce606060ca
jungeun <kje2066@naver.com>
2025-08-21 15:24:07 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/com/shinhanDS5gi/memento/MementoApplication.java
+++ src/main/java/com/shinhanDS5gi/memento/MementoApplication.java
@@ -2,7 +2,9 @@ package com.shinhanDS5gi.memento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MementoApplication {
 
