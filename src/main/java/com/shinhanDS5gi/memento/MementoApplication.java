581ef1c9cb51af9eb5b916d393dadc1bb637786b
KimKido <rleh9255@naver.com>
2025-08-22 11:48:36 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/com/shinhanDS5gi/memento/MementoApplication.java
+++ src/main/java/com/shinhanDS5gi/memento/MementoApplication.java
@@ -2,11 +2,14 @@ package com.shinhanDS5gi.memento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MementoApplication {
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(MementoApplication.class, args);
 	}
 
