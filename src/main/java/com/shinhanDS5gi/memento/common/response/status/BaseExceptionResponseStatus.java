8feb8773e583eff3461b083cd4bdc5e959df40ba
KimKido <rleh9255@naver.com>
2025-08-25 01:33:02 +0900

    feat: 멘토스 수정하기 API 기능 구현 완료

--- src/main/java/com/shinhanDS5gi/memento/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/shinhanDS5gi/memento/common/response/status/BaseExceptionResponseStatus.java
@@ -11,10 +11,18 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
      */
     SUCCESS(1000,HttpStatus.OK.value(), "요청에 성공하였습니다."),
     FAILURE(2000, HttpStatus.BAD_REQUEST.value(), "요청에 실패하였습니다."),
+
+    /**
+     * Mentos 관련 code : 2500 대
+     */
+    CANNOT_FOUND_MENTOS(2500, HttpStatus.NOT_FOUND.value(), "해당 멘토스 게시글을 찾을 수 없습니다."),
+    NO_AUTHORITY_TO_UPDATE(2501, HttpStatus.FORBIDDEN.value(), "게시글을 수정할 권한이 없습니다."),
+
     /**
      * Token 관련 code : 3000 대
      */
     INVALID_TOKEN(3000, HttpStatus.OK.value(), "유효하지 않은 토큰입니다.");
+
 
 
 
