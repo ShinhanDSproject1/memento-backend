728e260959c5b93a3c325bddfe525239a0a39e5c
최다희 <jedihappy11@gmail.com>
2025-08-26 16:55:42 +0900

    fix: 로그인 @AllArgsConstructor 추가

--- src/main/java/com/shinhanDS5gi/memento/dto/auth/LoginResponse.java
+++ src/main/java/com/shinhanDS5gi/memento/dto/auth/LoginResponse.java
@@ -1,10 +1,13 @@
 package com.shinhanDS5gi.memento.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@AllArgsConstructor
 //로그인 Response DTO
 public class LoginResponse {
     private String memberName;
