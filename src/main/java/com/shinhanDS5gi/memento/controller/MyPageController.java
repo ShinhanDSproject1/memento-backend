2604d16391023c8ad6428097ec044923781a91de
KimKido <rleh9255@naver.com>
2025-08-27 00:31:23 +0900

    feat : 나의 프로필 정보 조회 API 기능 구현 완료

--- src/main/java/com/shinhanDS5gi/memento/controller/MyPageController.java
+++ src/main/java/com/shinhanDS5gi/memento/controller/MyPageController.java
@@ -2,13 +2,12 @@ package com.shinhanDS5gi.memento.controller;
 
 import com.shinhanDS5gi.memento.common.response.BaseResponse;
 import com.shinhanDS5gi.memento.dto.CreateReviewRequest;
+import com.shinhanDS5gi.memento.dto.MyProfileResponse;
+import com.shinhanDS5gi.memento.service.MyPageService;
 import com.shinhanDS5gi.memento.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.shinhanDS5gi.memento.common.response.status.BaseExceptionResponseStatus.SUCCESS;
 
@@ -19,6 +18,7 @@ import static com.shinhanDS5gi.memento.common.response.status.BaseExceptionRespo
 public class MyPageController {
 
     private final ReviewService reviewService;
+    private final MyPageService myPageService;
 
     /* 리뷰 작성하기 */
     @PostMapping("/reviews")
@@ -26,5 +26,13 @@ public class MyPageController {
         Long currentMemberId = 1L;
         reviewService.createReview(currentMemberId, requestDto);
         return new BaseResponse<>(SUCCESS, null);
+    }
+
+    /* 나의 프로필 조회하기 */
+    @GetMapping("/profile")
+    public BaseResponse<MyProfileResponse> getMyProfile() {
+        Long currentMemberId = 1L;
+        MyProfileResponse profile = myPageService.getMyProfile(currentMemberId);
+        return new BaseResponse<>(SUCCESS, profile);
     }
 }
---
+++ src/main/java/com/shinhanDS5gi/memento/service/MyPageServiceImpl.java
@@ -0,0 +1,34 @@
+package com.shinhanDS5gi.memento.service;
+
+import com.shinhanDS5gi.memento.common.exception.MemberException;
+import com.shinhanDS5gi.memento.domain.member.Member;
+import com.shinhanDS5gi.memento.dto.MyProfileResponse;
+import com.shinhanDS5gi.memento.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.shinhanDS5gi.memento.common.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_MEMBER;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MyPageServiceImpl implements MyPageService {
+
+    private final MemberRepository memberRepository;
+    
+    /* 나의 프로필 정보 조회 */
+    @Override
+    public MyProfileResponse getMyProfile(Long memberSeq) {
+
+        Member member = memberRepository.findById(memberSeq)
+                .orElseThrow(() -> new MemberException(CANNOT_FOUND_MEMBER));
+
+        return MyProfileResponse.builder()
+                .memberName(member.getMemberName())
+                .memberPhoneNumber(member.getMemberPhoneNumber())
+                .memberBirthDate(member.getMemberBirthDate())
+                .memberId(member.getMemberId())
+                .build();
+    }
+}
