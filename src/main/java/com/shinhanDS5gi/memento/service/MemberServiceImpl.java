7879c5b6ecbec693815e3ecc7e26839c40ff2174
최다희 <jedihappy11@gmail.com>
2025-08-27 08:58:42 +0900

    fix: 사용하지 않는 import문 삭제

--- src/main/java/com/shinhanDS5gi/memento/service/MemberServiceImpl.java
+++ src/main/java/com/shinhanDS5gi/memento/service/MemberServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.beans.DirectFieldAccessor;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.DirectFieldAccessor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
