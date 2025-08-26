31382bfa92e426997c87170e764f9172531a50b0
jungeun <kje2066@naver.com>
2025-08-26 15:12:12 +0900

    feat: AdminController.expelMemberByAdmin 작성

--- src/main/java/com/shinhanDS5gi/memento/controller/AdminController.java
+++ src/main/java/com/shinhanDS5gi/memento/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.shinhanDS5gi.memento.controller;
 import com.shinhanDS5gi.memento.common.response.BaseResponse;
 import com.shinhanDS5gi.memento.dto.CreateReportRequest;
 import com.shinhanDS5gi.memento.dto.SelectReportResponse;
+import com.shinhanDS5gi.memento.service.MemberService;
 import com.shinhanDS5gi.memento.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import static com.shinhanDS5gi.memento.common.response.status.BaseExceptionRespo
 public class AdminController {
 
     private final ReportService reportService;
+    private final MemberService memberService;
     
     /* 신고 작성하기 */
     @PostMapping("/reports")
@@ -40,5 +42,14 @@ public class AdminController {
     public BaseResponse<SelectReportResponse> getReportById(@PathVariable("reportSeq") Long reportSeq) {
         SelectReportResponse report = reportService.findReportById(reportSeq);
         return new BaseResponse<>(report);
+    }
+
+    /* 회원 제명하기(관리자) */
+    @PatchMapping("/member/{memberSeq}")
+    public BaseResponse<Void> expelMemberByAdmin(@PathVariable("memberSeq") Long memberSeq){
+        log.info("[AdminController.expelMemberByAdmin]");
+        // 여기에 member 검증하는 부분이 나와야함
+        memberService.expelMemberByAdmin(memberSeq);
+        return new BaseResponse<>(null);
     }
 }
