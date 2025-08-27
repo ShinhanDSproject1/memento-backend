165d1f53e87c8768ae057fdc19ad84810713e5aa
jungeun <kje2066@naver.com>
2025-08-27 10:28:57 +0900

    fix: Jackson 직렬화 규칙때문에 isApproved->approved 로 수정

--- src/main/java/com/shinhanDS5gi/memento/dto/mentos/GetMentosListResponse.java
+++ src/main/java/com/shinhanDS5gi/memento/dto/mentos/GetMentosListResponse.java
@@ -1,5 +1,6 @@
 package com.shinhanDS5gi.memento.dto.mentos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
