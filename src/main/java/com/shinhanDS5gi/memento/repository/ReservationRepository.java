b6915a0f3d2fc1941350a70134d819c44c7d234c
KimKido <rleh9255@naver.com>
2025-09-02 12:38:20 +0900

    fix : 모든 신고 내역 조회 시 신고 status가 pending 상태인 신고 내역만 조회 조건 추가

--- src/main/java/com/shinhanDS5gi/memento/repository/ReservationRepository.java
+++ src/main/java/com/shinhanDS5gi/memento/repository/ReservationRepository.java
@@ -1,8 +1,6 @@
 package com.shinhanDS5gi.memento.repository;
 
 import com.shinhanDS5gi.memento.domain.Reservation;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
