5049ab9e0b66e4b2db078ca5ea0b515a025126e2
KimKido <rleh9255@naver.com>
2025-09-02 15:50:14 +0900

    feat : 나의 프로필 조회 시 접속 유저 타입에 따라 Mento일 경우 '보유 자격증 목록' 추가 조회, Menti일 경우 제외 후 조회하는 API 구현 완료

--- src/main/java/com/shinhanDS5gi/memento/dto/mypage/MyProfileResponse.java
+++ src/main/java/com/shinhanDS5gi/memento/dto/mypage/MyProfileResponse.java
@@ -1,9 +1,12 @@
 package com.shinhanDS5gi.memento.dto.mypage;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.shinhanDS5gi.memento.dto.mento.MentoCertificationsResponse;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /* 나의 프로필 조회를 위한 응답 DTO*/
 @Getter
@@ -14,4 +17,8 @@ public class MyProfileResponse {
     private final String memberPhoneNumber;
     private final LocalDate memberBirthDate;
     private final String memberId;
+    private final String memberType;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY) // 리스트가 비어있으면 JSON 응답에서 제외
+    private final List<MentoCertificationsResponse> certifications;
 }
