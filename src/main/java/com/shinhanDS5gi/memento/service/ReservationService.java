d96fc33a6faee2d8a52e7e1a307aa13bce366d2f
jungeun <kje2066@naver.com>
2025-09-05 12:23:19 +0900

    feat: ReservationService 구현
    mysql reservation 테이블에서 예약이 완료된 애들의 시간을 불러오기 위함

---
+++ src/main/java/com/shinhanDS5gi/memento/service/ReservationService.java
@@ -0,0 +1,10 @@
+package com.shinhanDS5gi.memento.service;
+
+import com.shinhanDS5gi.memento.dto.reservation.GetAvailableDateResponse;
+
+public interface ReservationService {
+
+    /* 선택한 날짜에 예약이 가능한 시간대 보여주기 */
+    GetAvailableDateResponse getAvailableTime(Long mentosSeq, Long memberSeq, String selectedDate);
+
+}
