6eaef921778c0bb777a5f9737dafaa24679df477
jungeun <kje2066@naver.com>
2025-09-05 12:22:01 +0900

    feat: GetAvailableDateResponse dto 작성

---
+++ src/main/java/com/shinhanDS5gi/memento/dto/reservation/GetAvailableDateResponse.java
@@ -0,0 +1,17 @@
+package com.shinhanDS5gi.memento.dto.reservation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class GetAvailableDateResponse {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private List<String> availableTime;
+}
