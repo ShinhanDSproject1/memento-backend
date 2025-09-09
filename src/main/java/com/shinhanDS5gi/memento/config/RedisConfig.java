8fd1f7840546fb18619cc0716d00ceb072c0d766
최다희 <jedihappy11@gmail.com>
2025-09-09 20:10:35 +0900

    feat: jwt 발급을 위한 환경변수 설정

--- src/main/java/com/shinhanDS5gi/memento/config/RedisConfig.java
+++ src/main/java/com/shinhanDS5gi/memento/config/RedisConfig.java
@@ -4,9 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
