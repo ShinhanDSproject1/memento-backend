15003c4d5a07ca65397b320f3564b39a746c9aa1
KimKido <rleh9255@naver.com>
2025-09-17 22:15:37 +0900

    fix : import문 제거

--- src/main/java/com/shinhanDS5gi/memento/dto/mento/CreateMentoProfileRequest.java
+++ src/main/java/com/shinhanDS5gi/memento/dto/mento/CreateMentoProfileRequest.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Getter
