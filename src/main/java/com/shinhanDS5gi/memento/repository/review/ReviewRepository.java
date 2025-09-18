9fd5389be721a93d24c1b5bd2667680da94c7d2a
jungeun <kje2066@naver.com>
2025-09-18 12:47:37 +0900

    feat: 회원탈퇴 시 리뷰 inactive 처리 완료

--- src/main/java/com/shinhanDS5gi/memento/repository/review/ReviewRepository.java
+++ src/main/java/com/shinhanDS5gi/memento/repository/review/ReviewRepository.java
@@ -3,7 +3,6 @@ package com.shinhanDS5gi.memento.repository.review;
 import com.shinhanDS5gi.memento.domain.Review;
 
 import com.shinhanDS5gi.memento.domain.base.BaseStatus;
-import com.shinhanDS5gi.memento.dto.mento.MentoReviewsListResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.lettuce.core.dynamic.annotation.Param;
@@ -14,11 +13,23 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository {
 
-//    @Modifying(clearAutomatically = true, flushAutomatically = true)
-//    @Query("update Review r set r.status = :afterStatus where r.member.memberSeq = :memberSeq and r.status = :beforeStatus")
-//    int updateReviewStatus(@Param("memberSeq") Long memberSeq,
-//                           @Param("afterStatus") BaseStatus afterStatus,
-//                           @Param("beforeStatus") BaseStatus beforeStatus);
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+    update Review r
+       set r.status = :afterStatus
+     where r.status = :beforeStatus
+       and r.reservation.reservationSeq in (
+            select rs.reservationSeq
+              from Reservation rs
+             where rs.member.memberSeq = :memberSeq
+               and rs.status = :beforeStatus
+       )
+""")
+    int updateReviewStatus(
+            @Param("memberSeq") Long memberSeq,
+            @Param("afterStatus") BaseStatus afterStatus,
+            @Param("beforeStatus") BaseStatus beforeStatus);
+
 
 
 }
